380667287f530de844c23697fd7008b0eafe3461
justincrywolf <justincrywolf@gmail.com>
2018-05-15 12:03:41 -0400

    building search field for navbar

---
+++ cella/client/src/components/NavSearch/NavSearch.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const NavSearch = props => (
+    <form className="search">
+        <input
+            value={props.search}
+            onChange={props.handleInputChange}
+            name="searchAddress"
+            type="text"
+            className="form-control"
+            placeholder="Seach"    
+        />
+        <button
+            type="submit"
+            onClick={props.handleFormSubmit}
+            className="btn btn-success"
+        >Submit
+        </button>
+    </form>
+);
+
+export default NavSearch;
