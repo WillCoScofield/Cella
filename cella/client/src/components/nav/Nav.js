1cd1bbc1971f336f1399f05876242ce10b73c202
justincrywolf <justincrywolf@gmail.com>
2018-05-15 11:15:56 -0400

    updating navbar with links and a mostly empty css file

--- cella/client/src/components/nav/Nav.js
+++ cella/client/src/components/nav/Nav.js
@@ -1,11 +1,52 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 
-const Nav = () => (
-  <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-    <a className="navbar-brand" href="/">
+// Depending on the current path, this component sets the "active" class on the appropriate navigation link item
+const Navbar = props => (
+  <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <Link className="navbar-brand" to="/">
       Cella
-    </a>
+    </Link>
+    <div>
+      <ul className="navbar-nav">
+        <li
+          className={
+            window.location.pathname === "/" ||
+            window.location.pathname === "/link1"
+              ? "nav-item active"
+              : "nav-item"
+          }
+        >
+          <Link to="/" className="nav-link">
+            link1
+          </Link>
+        </li>
+        <li
+          className={
+            window.location.pathname === "/link2"
+              ? "nav-item active"
+              : "nav-item"
+          }
+        >
+          <Link to="/link2" className="nav-link">
+            link2
+          </Link>
+        </li>
+        <li
+          className={
+            window.location.pathname === "/link3"
+              ? "nav-item active"
+              : "nav-item"
+          }
+        >
+          <Link to="/link3" className="nav-link">
+            link3
+          </Link>
+        </li>
+      </ul>
+    </div>
   </nav>
 );
 
-export default Nav;
+export default Navbar;
