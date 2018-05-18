babb481cee78123ff9d8ee5456d5eee375974088
Connor <wscofield@elon.edu>
2018-05-18 13:34:23 -0400

    fixed Search Page.js

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -10,57 +10,10 @@ import "../components/BodyContainer/BodyContainer.css"
 
 
 class Search extends Component {
-    state = {
-      listName: "Warehouse",
-      listLocation: "Atlanta",
-      listImage: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0IyjG9GY-DYRCUxS9wYvIdjA6rzhjj8YJisFfyMmIfU0zmIKwMw"
-    };
-  
-    // When the component mounts, get a list of all available base breeds and update this.state.breeds
-    // componentDidMount() {
-    //   API.getBaseBreedsList()
-    //     .then(res => this.setState({ breeds: res.data.message }))
-    //     .catch(err => console.log(err));
-    // }
-  
-    // handleInputChange = event => {
-    //   this.setState({ search: event.target.value });
-    // };
-  
-    // handleFormSubmit = event => {
-    //   event.preventDefault();
-    //   API.getDogsOfBreed(this.state.search)
-    //     .then(res => {
-    //       if (res.data.status === "error") {
-    //         throw new Error(res.data.message);
-    //       }
-    //       this.setState({ results: res.data.message, error: "" });
-    //     })
-    //     .catch(err => this.setState({ error: err.message }));
-    // };
-
-
-    render() {
-      return (
-     
-        <div>
-           
-           
-                <div className="row">
-                    <div className="col-sm-6">
-                    <SearchResultsContainer/>
-                    </div>
-                  
-                    <div className="col-sm-6">
-                    <MapRender />
-                
-
-                    </div>
-                </div>
-
-        
-
-
+  state = {
+    listName: "Warehouse",
+    listLocation: "Atlanta",
+    listImage: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0IyjG9GY-DYRCUxS9wYvIdjA6rzhjj8YJisFfyMmIfU0zmIKwMw"
   };
 
   // When the component mounts, get a list of all available base breeds and update this.state.breeds
@@ -91,21 +44,18 @@ class Search extends Component {
     return (
       <BodyContainer id="search-page">
 
-        <div class="row">
-          <div class="col-sm-6">
+        <div className="row">
+          <div className="col-sm-6">
             <SearchResultsContainer />
           </div>
 
-          <div class="col-sm-6">
-            <Map
-              image={this.state.map}
-            />
+          <div className="col-sm-6">
+            <MapRender />
+
 
           </div>
         </div>
-
       </BodyContainer>
-
     );
   }
 }
