ae2a1a2a2bbdb68ec31b6f8bc50356d152ea59eb
Connor <wscofield@elon.edu>
2018-05-23 10:13:42 -0400

    added more example data to the seed script

--- models/list.js
+++ models/list.js
@@ -3,15 +3,16 @@ const Schema = mongoose.Schema;
 
 const listSchema = new Schema({
     type: { type: String, required: true },
-    image: {type: String, required: false },
+    image: { type: String, required: false },
     SquareFeet: { type: Number, required: true },
     Price: { type: Number, required: true },
     Address: { type: String, required: true },
     Description: { type: String, required: true },
     YearBuilt: { type: String, required: true },
-    Features: {type: String, required: true},
-    Latitude: {type: Number, required: true},
-    Longitude: {type: Number, required: true},
+    Features: { type: String, required: true },
+    Latitude: { type: Number, required: true },
+    Longitude: { type: Number, required: true },
+    Owner: { type: String, required: false },
     date: { type: Date, default: Date.now }
 });
 
