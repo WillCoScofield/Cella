c3dc7bfe9e1725a9487424af94d0a602a1727907
Siyan Zhou <siyanzhou@lawn-128-61-30-243.lawn.gatech.edu>
2018-05-23 13:53:11 -0400

    refactored code to use URLSearchParams instead of query-string package

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -7,8 +7,6 @@ import BodyContainer from "../components/BodyContainer/BodyContainer";
 import "../components/BodyContainer/BodyContainer.css"
 import SearchFilter from "../components/SearchFilter/SearchFilter"
 import API from "../utils/API";
-import { parse as parseQuery } from 'query-string';
-
 
 class Search extends Component {
   state = {
@@ -18,7 +16,8 @@ class Search extends Component {
 
   componentDidMount() {
     this.loadListings();
-    const { searchAddress } = parseQuery(this.props.location.search);
+    const searchQuery = new URLSearchParams(this.props.location.search);
+    const searchAddress = searchQuery.get("searchAddress");
     this.getLocations(searchAddress);
   };
 
